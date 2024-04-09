72fe129746f545b2f9308b46791b062d7c11ab57
David Carr <dave@dcblog.dev>
2024-04-09 21:17:31 +0100

    set image size 100%

--- Modules/Dogs/resources/views/livewire/index.blade.php
+++ Modules/Dogs/resources/views/livewire/index.blade.php
@@ -3,7 +3,7 @@
 
     @if (request()->path() == '/')
         <div id="featured">
-            <div class="container max-w-screen-lg mx-auto px-8 py-12 mb-12">
+            <div class="mx-auto max-w-2xl">
 
                 <div class="py-5 mx-auto block px-4 w-full">
                     <h1 class="mb-10 font-bold text-5xl text-black">
